0f5d453a8af47093330044a22a06c5827a1a0e05
Mark Condello <mark@dcode.com.au>
2021-07-25 14:37:00 +1000

    - skipped ahead to section 3 code start

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,17 @@ module.exports = {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
+
     theme: {
         extend: {
             colors: {
-                'grey-background': '#f7f8fc',
                 transparent: 'transparent',
                 current: 'currentColor',
+
                 black: colors.black,
                 white: colors.white,
                 gray: colors.trueGray,
+                'gray-background': '#f7f8fc',
                 'blue': '#328af1',
                 'blue-hover': '#2879bd',
                 'yellow' : '#ffc73c',
@@ -23,26 +25,36 @@ module.exports = {
                 'green' : '#1aab8b',
                 'purple' : '#8b60ed',
             },
-            maxWidth:{
-                custom: '62.5rem',
-            },
             spacing: {
+                22: '5.5rem',
+                44: '11rem',
                 70: '17.5rem',
+                76: '19rem',
+                104: '26rem',
                 175: '43.75rem',
             },
+            maxWidth: {
+                custom: '68.5rem',
+            },
+            boxShadow: {
+                card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
+                dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)',
+            },
             fontFamily: {
-                sans: ['Open sans', ...defaultTheme.fontFamily.sans],
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
             },
             fontSize: {
                 xxs: ['0.625rem', { lineHeight: '1rem' }],
             },
         },
     },
+
     variants: {
         extend: {
             opacity: ['disabled'],
         },
     },
+
     plugins: [
         require('@tailwindcss/forms'),
         require('@tailwindcss/line-clamp'),
